eb493d97dc25d02d7cb65a18b969402b5c11dc08
Sabrina Belloni <sabrina82belloni@gmail.com>
2025-01-16 12:31:36 +0000

    added meta class to post

--- blog/models.py
+++ blog/models.py
@@ -24,6 +24,12 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True) #The auto_now sets the value to the current date and time whenever the record is saved, not just when it is created.
+    class Meta: #this is an optional class and it is used for data not inclused in the table fields
+        ordering = ["-created_on", "author"] #the hash symbol make it reverse newer to older data creation
+    #The __str__() method has changed this post identifier to a string literal. By passing self as an argument to the __str__() method,
+    # you can use the field values in the f-string and you can see the title instead of post object(n)
+    def __str__(self):
+        return f"the title of this post is {self.title} | made by {self.author}"
 
 
 class Comment(models.Model):
