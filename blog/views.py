cae4a1cf582878424da1e4f007d2ee03d291b61a
Sabrina Belloni <sabrina82belloni@gmail.com>
2025-01-17 12:12:06 +0000

    add previous pagination button

--- blog/views.py
+++ blog/views.py
@@ -13,4 +13,8 @@ class PostList(generic.ListView):
     #When you change all() to filter(author=1) on the queryset line filtering by the author ID of 1
     #The User model assigns primary key (PK) IDs to all users by default (1 is the superuser)
     template_name = "blog/index.html"
-    paginate_by = 6
+    paginate_by = 6 # pagination settings control = six post per page
+    #When we set the paginate_by property in the ListView class, Django adds two items to the data that is passed through to the template: a boolean value, 
+    # is_paginated and a dictionary object called page_obj, which we just call the Page object.
+    #For is_paginated, Django counts the number of objects, or records, returned by our queryset. If that is more than the number set in the paginate_by property,
+    #  then is_paginated is set to True otherwise it is set to False.
